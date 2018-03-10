75632130290b100b05a4bc6b2d9d1f55f978a163
Ian Lee <metaforrest91@gmail.com>
2018-03-10 03:28:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 function cl(x){console.log(x)}
 var express = require('express');
-var app = express();
-
+const mongodb = require('mongodb');
+var app = express(), uri = process.env.URL, db;
 
 
 app.get("/", function (request, response) {
-  response.send('<h1>image search</h1><br><p>search for a specific phrase by pointing to /search/{phrase}</p><br>get a list of wedding events');
+  response.send('<h1>image search api</h1><br><p>search for a specific phrase by pointing to /search/{phrase to search for}</p><br><p>get the latest searches by pointing to /latest</p><br><p>paginate by querying ?offset=2, ?offset=3, etc.');
 });
+
 
 app.get('/search/:img', function (req, res) {
   
   var page = 1;
   var responseArray = [];
+  var recentSearches = db.collection('recent-searches');
+  
+  recentSearches.insert({phrase : req.params.img, timeStamp : new Date().toString()}, function(err) {if (err) throw err;})
   
   if (req.query.offset) {
-    page = req.query.page;
+    page = req.query.offset;
   }
   
   var options = {
@@ -33,7 +37,7 @@ app.get('/search/:img', function (req, res) {
       body = JSON.parse(body);
 
       for (let image of body.images) {
-        cl(image.display_sizes);
+
         let information = {
           imageURL : image.display_sizes[0].uri,
           altText : image.title,
@@ -41,13 +45,10 @@ app.get('/search/:img', function (req, res) {
         };
         
         responseArray.push(information);
-
-      
-
-    }
+      }
 
       res.send(responseArray);
-      }
+    }
     else {
       res.send(error, response.statusCode);
     }
@@ -56,38 +57,36 @@ app.get('/search/:img', function (req, res) {
 });
 
 
-app.get('/events', function (req, res) {
+app.get('/latest', function (req, res) {
 
   var page = 1;
   var responseArray;
   
   if (req.query.offset) {
-    page = req.query.page;
+    page = req.query.offset;
   }
   
-  var options = {
-    url: "https://api.gettyimages.com/v3/search/events?phrase=wedding&page="+page+"&page_size=10",
-    headers: {
-      'Api-Key': process.env.API_KEY
-    }
-  };
+  var recentSearches = db.collection('recent-searches');
   
- 
-  var request = require('request');
-  request(options, function (error, response, body) {
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    if (!error && response.statusCode == 200) {
-      
-      body = JSON.parse(body);
-      res.send(body);
-
-    }
-    else {
-      cl(error);
-      res.send(error, response.statusCode);
-    }
+  recentSearches.find({}, { _id : 0}, {
+    "sort": { "timeStamp": -1 },
+  }).toArray(function (err, result) {
+    if (err) throw err;
+    // paginate response
+    responseArray = result.slice((page*10)-10, page*10);
+    res.send(responseArray);
   });
   
+});
+
+
+// connect to the db before starting the app server
+mongodb.MongoClient.connect(uri, function(err, database) {
+  if (err) {console.log(err); process.exit(1);}
+
+  // save db obj from callback for reuse
+  db = database;
+  console.log('database connection ready ');
 });
 
 
